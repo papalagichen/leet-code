05f2e621d5c816c3bf7d72e3c53881373bba2e37
Jubing Chen <papalagi.chen@gmail.com>
2015-02-14 12:59:32 +0800

    Update

--- Two Sum.py
+++ Two Sum.py
@@ -8,4 +8,4 @@ class Solution:
             mem[rest] = i
 
 if __name__ == '__main__':
-    print("index1={}, index2={}".format(*Solution().twoSum((2, 7, 11, 15), 9)))
+    print((1, 2) == Solution().twoSum((2, 7, 11, 15), 9))
