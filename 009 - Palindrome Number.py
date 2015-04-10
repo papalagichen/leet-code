0e718461675cd83556cbe73d7433f8d56683656d
Jubing Chen <papalagi.chen@gmail.com>
2015-04-10 09:12:59 +0800

    Add Solution2 to "009 - Palindrome Number"

--- 009 - Palindrome Number.py
+++ 009 - Palindrome Number.py
@@ -11,11 +11,22 @@ class Solution:
             return 0
         return y * -1 if negative else y
 
+
+class Solution2:
+    def reverse(self, x):
+        negative = x < 0
+        x = int(str(abs(x))[::-1])
+        if x > 2 ** 31:
+            return 0
+        return x * -1 if negative else x
+
+
 if __name__ == '__main__':
     import Test
-    Test.test(0, Solution().reverse(0))
-    Test.test(321, Solution().reverse(123))
-    Test.test(-321, Solution().reverse(-123))
-    Test.test(1, Solution().reverse(100))
-    Test.test(101, Solution().reverse(101))
-    Test.test(0, Solution().reverse(1000000003))
+
+    Test.test(0, Solution2().reverse(0))
+    Test.test(321, Solution2().reverse(123))
+    Test.test(-321, Solution2().reverse(-123))
+    Test.test(1, Solution2().reverse(100))
+    Test.test(101, Solution2().reverse(101))
+    Test.test(0, Solution2().reverse(1000000003))
