5bb2bdcd42dfbf46607cb6400865c77e89a4d6df
Jubing Chen <papalagi.chen@gmail.com>
2015-04-10 17:38:12 +0800

    Update Test.test

--- Test.py
+++ Test.py
@@ -1,10 +1,12 @@
 def test(func, args_expects):
+    correct = True
     for args, expect in args_expects:
         if type(args) in (tuple, list):
             result = func(*args)
         else:
             result = func(args)
-        if expect == result:
-            print("OK")
-        else:
-            print("Expect {} but got {}".format(expect, result))
+        if expect != result:
+            correct = False
+            print("When input is {}, {} is expected but got {}".format(args, expect, result))
+    if correct:
+        print('OK!')
