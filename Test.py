f94b372bdc50e201e243bc755ee4dff37519d061
Jubing Chen <papalagi.chen@gmail.com>
2015-04-10 23:40:38 +0800

    Update Test.test to calculate number of args of function

--- Test.py
+++ Test.py
@@ -1,12 +1,16 @@
+import copy
+import inspect
+
+
 def test(func, args_expects):
     correct = True
     for args, expect in args_expects:
-        if type(args) in (tuple, list):
-            result = func(*args)
+        if type(args) in (list, tuple) and len(args) == len(inspect.getargspec(func).args) - 1:
+            result = func(*copy.deepcopy(args))
         else:
-            result = func(args)
+            result = func(copy.deepcopy(args))
         if expect != result:
             correct = False
-            print("When input is {}, {} is expected but got {}".format(args, expect, result))
+            print("When input is '{}', '{}' is expected but got '{}'".format(args, expect, result))
     if correct:
         print('OK!')
