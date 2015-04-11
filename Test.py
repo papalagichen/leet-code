c82a7dbc6d61b2d124d2c3a8f6fe510327ac7bfe
Jubing Chen <papalagi.chen@gmail.com>
2015-04-12 03:46:10 +0800

    Add parameter "copy_parameters" to Test.test

--- Test.py
+++ Test.py
@@ -2,13 +2,15 @@ import copy
 import inspect
 
 
-def test(func, args_expects):
+def test(func, args_expects, copy_parameters=True):
     correct = True
     for args, expect in args_expects:
+        if copy_parameters:
+            args = copy.deepcopy(args)
         if type(args) in (list, tuple) and len(args) == len(inspect.getargspec(func).args) - 1:
-            result = func(*copy.deepcopy(args))
+            result = func(*args)
         else:
-            result = func(copy.deepcopy(args))
+            result = func(args)
         if expect != result:
             correct = False
             print("When input is '{}', '{}' is expected but got '{}'".format(args, expect, result))
