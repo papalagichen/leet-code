9fac97a7436db8e2b38d301d3d8bd341f4efde95
Jubing Chen <papalagi.chen@gmail.com>
2015-04-21 11:09:30 +0800

    Add ListNode.index
    Redesign __cmp__, __eq__ of ListNode.

--- ListBuilder.py
+++ ListBuilder.py
@@ -4,6 +4,15 @@ class ListNode:
         self.next = None
 
     def __str__(self):
+        return self.to_string()
+
+    def __cmp__(self, other):
+        return cmp(self.to_string(), other.to_string())
+
+    def __eq__(self, other):
+        return self.val == other.val
+
+    def to_string(self):
         s = ''
         p = self
         while p:
@@ -13,15 +22,30 @@ class ListNode:
             p = p.next
         return s
 
-    def __cmp__(self, other):
-        return cmp(str(self), str(other))
+    def index(self, n=1):
+        p = self
+        while n and p:
+            p = p.next
+            n -= 1
+        return p
 
 
 def build(*a):
     if a:
-        head = p = ListNode(a[0])
-        for n in a[1:]:
+        head = p = ListNode('dummy')
+        for n in a:
             p.next = ListNode(n)
             p = p.next
-        return head
+        return head.next
     return None
+
+
+if __name__ == '__main__':
+    import Test
+
+    list1 = build(1, 2, 3, 4, 5)
+    list2 = build(1, 2, 3, 4, 5)
+    list3 = build(2, 3, 4, 5, 6)
+
+    Test.cmp_equal(list1, list2)
+    Test.equal(ListNode(3), list1.index(2))
