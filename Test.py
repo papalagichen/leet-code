396da8ff5a76e70ec8530741378f8ff3dd3f30a8
Jubing Chen <papalagi.chen@gmail.com>
2015-04-21 11:12:11 +0800

    Add Test.cmp_equal
    Aceept test target takes no arguments.

--- Test.py
+++ Test.py
@@ -13,12 +13,27 @@ def test(funcs, args_expects, copy_parameters=True):
                 args = copy.deepcopy(args)
             if type(args) in (list, tuple) and len(args) > 1 and len(args) == len(inspect.getargspec(func).args) - 1:
                 result = func(*args)
-            elif args:
-                result = func(args)
-            else:
+            elif args is None and len(inspect.getargspec(func).args) == 1:
                 result = func()
+            else:
+                result = func(args)
             if expect != result:
                 correct = False
-                print("When calling {} with input '{}', '{}' is expected but got '{}'".format(func, args, expect, result))
+                print("When calling {} with input '{}', '{}' is expected but got '{}'".format(
+                    func, args, expect, result))
     if correct:
+        print('OK!')
+
+
+def equal(expect, given):
+    if expect == given:
+        print('OK!')
+    else:
+        print("Expect: '{}' Given: '{}'".format(expect, given))
+
+
+def cmp_equal(expect, given):
+    if cmp(expect, given):
+        print("Expect: '{}' Given: '{}'".format(expect, given))
+    else:
         print('OK!')
