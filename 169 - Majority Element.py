7b4b152367bcb44aad73b835aea5ac733f107133
Jubing Chen <papalagi.chen@gmail.com>
2015-04-23 01:30:59 +0800

    Add "169 - Majority Element"

---
+++ 169 - Majority Element.py
@@ -0,0 +1,26 @@
+class Solution:
+    def majorityElement(self, nums):
+        half_length = len(nums) / 2
+        d = {}
+        for i in nums:
+            if i in d:
+                d[i] += 1
+                if d[i] > half_length:
+                    return i
+            else:
+                d[i] = 1
+        return nums[0]
+
+
+class Solution2:
+    def majorityElement(self, nums):
+        return sorted(nums)[(len(nums) / 2)]
+
+
+if __name__ == '__main__':
+    import Test
+
+    Test.test((Solution().majorityElement, Solution2().majorityElement), [
+        ([2], 2),
+        ([1, 1, 1, 2, 3], 1),
+    ])
