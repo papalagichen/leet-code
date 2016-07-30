1464f4aeed6b2a9a59092136c4eebcacd33b56c8
Jubing Chen <papalagi.chen@gmail.com>
2016-07-30 12:11:54 +0800

    Add "290 - Word Pattern"

---
+++ 290 - Word Pattern.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def wordPattern(self, pattern, str):
+        tokens = str.split(' ')
+        if len(tokens) != len(pattern):
+            return False
+        d = {}
+        for i in xrange(len(pattern)):
+            if pattern[i] in d and d[pattern[i]] != tokens[i] or tokens[i] in d.values() and pattern[i] not in d:
+                return False
+            d[pattern[i]] = tokens[i]
+        return True
+
+
+if __name__ == '__main__':
+    import Test
+
+    Test.test(Solution().wordPattern, [
+        (('abba', 'dog cat cat dog'), True),
+        (('abba', 'dog cat cat fish'), False),
+        (('aaaa', 'dog cat cat dog'), False),
+        (('abba', 'dog dog dog dog'), False),
+        (('jquery', 'jquery'), False)
+    ])
