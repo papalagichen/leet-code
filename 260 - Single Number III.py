7255c5dbfe17c773c77bbab54412199d69c1e292
Jubing Chen <papalagi.chen@gmail.com>
2016-08-02 01:30:33 +0800

    Update "260 - Single Number III"

--- 260 - Single Number III.py
+++ 260 - Single Number III.py
@@ -16,10 +16,27 @@ class Solution(object):
         return result + nums[len(result) - 2:]
 
 
+# https://discuss.leetcode.com/topic/25382/sharing-explanation-of-the-solution
+class Solution2(object):
+    def singleNumber(self, nums):
+        xor, a, b = 0, 0, 0
+        for num in nums:
+            xor ^= num
+        mask = 1
+        while xor & mask == 0:
+            mask <<= 1
+        for num in nums:
+            if num & mask:
+                a ^= num
+            else:
+                b ^= num
+        return [a, b]
+
+
 if __name__ == '__main__':
     import Test
 
-    Test.test(Solution().singleNumber, [
+    Test.test((Solution().singleNumber, Solution2().singleNumber), [
         ([1, 2, 1, 3, 2, 5], [3, 5]),
         ([0, 1, 1, 2], [0, 2]),
     ])
