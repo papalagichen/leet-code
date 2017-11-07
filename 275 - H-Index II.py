c8ace6e1f5b2663b709f573aa0963cf58c580d43
Jubing Chen <papalagi.chen@gmail.com>
2017-11-07 09:06:54 +0800

    Add "275 - H-Index II"

---
+++ 275 - H-Index II.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def hIndex(self, citations):
+        """
+        :type citations: List[int]
+        :rtype: int
+        """
+        h = 0
+        for c in citations[::-1]:
+            if c > 0:
+                if c > h:
+                    h += 1
+                else:
+                    break
+        return h
+
+
+if __name__ == '__main__':
+    import Test
+
+    Test.test(Solution().hIndex, [
+        ([0, 1, 3, 5, 6], 3),
+        ([0, 1, 3, 6], 2),
+        ([5], 1),
+        ([5, 6], 2),
+        ([0], 0),
+    ])
