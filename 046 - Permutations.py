2152243b66dc4d5967b9cf2b0c0a66619b08a01f
Jubing Chen <papalagi.chen@gmail.com>
2019-08-21 01:08:22 -0700

    Add "046 - Permutations"

---
+++ 046 - Permutations.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        results = []
+        self.p(nums, 0, results)
+        return results
+
+    def p(self, nums, left, results):
+        if left == len(nums) - 1:
+            results.append(nums.copy())
+        else:
+            for i in range(left, len(nums)):
+                nums[left], nums[i] = nums[i], nums[left]
+                self.p(nums, left + 1, results)
+                nums[left], nums[i] = nums[i], nums[left]
