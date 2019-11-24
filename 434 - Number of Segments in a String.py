5b272b5241388782fbbd1158e39571e2a8ad3f34
Jubing Chen <papalagi.chen@gmail.com>
2019-11-23 23:27:56 -0800

    Update 434

--- 434 - Number of Segments in a String.py
+++ 434 - Number of Segments in a String.py
@@ -4,13 +4,40 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        return len(filter(lambda x: len(x) > 0, s.split(' ')))
+        return len(list(filter(lambda x: x, s.split(' '))))
+
+
+class Solution2(object):
+    def countSegments(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+
+        is_space = True
+        count = 0
+
+        for i in s:
+            if i == ' ':
+                if not is_space:
+                    is_space = True
+            else:
+                if is_space:
+                    is_space = False
+                    count += 1
+        return count
 
 
 if __name__ == '__main__':
     import Test
 
     Test.test(Solution().countSegments, [
+        ("", 0),
+        ("Hello, my name is John", 5),
+        ("Hello, my name   is    John", 5),
+    ])
+
+    Test.test(Solution2().countSegments, [
         ("", 0),
         ("Hello, my name is John", 5),
         ("Hello, my name   is    John", 5),
