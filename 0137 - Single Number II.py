3a04883fb852bc04d9bf6c1e6d866dc956e83be6
Jubing Chen <papalagi.chen@gmail.com>
2019-11-24 00:04:00 -0800

    Update 0137

--- 0137 - Single Number II.py
+++ 0137 - Single Number II.py
@@ -1,5 +1,8 @@
+from typing import List
+
+
 class Solution:
-    def singleNumber(self, nums):
+    def singleNumber(self, nums: List[int]) -> int:
         nums.sort()
         for i in range(0, len(nums) - 1, 3):
             if nums[i] != nums[i + 1]:
