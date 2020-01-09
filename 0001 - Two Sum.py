4537dee5347f2458015ada5fc9b8c0c354fd4d17
Jubing Chen <papalagi.chen@gmail.com>
2020-01-08 21:15:56 -0800

    Add Solution2 to "0001 - Two Sum"

--- 0001 - Two Sum.py
+++ 0001 - Two Sum.py
@@ -1,16 +1,25 @@
 class Solution:
-    def twoSum(self, num, target):
+    def twoSum(self, nums, target):
         mem = {}
-        for i, n in enumerate(num):
+        for i, n in enumerate(nums):
             if n in mem:
                 return mem[n], i
             rest = target - n
             mem[rest] = i
 
 
+class Solution2:
+    def twoSum(self, nums, target):
+        mem = {}
+        for i, n in enumerate(nums):
+            if target - n in mem:
+                return mem[target - n], i
+            mem[n] = i
+
+
 if __name__ == '__main__':
     import Test
 
-    Test.test(Solution().twoSum, [
+    Test.test([Solution().twoSum, Solution2().twoSum], [
         (((2, 7, 11, 15), 9), (0, 1)),
     ])
