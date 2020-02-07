34eb4e4fd40e09611fa7f3a98b0cc4a3fbffea82
Jubing Chen <papalagi.chen@gmail.com>
2020-02-06 23:25:55 -0800

    Update "0278 - First Bad Version.py"

--- 0278 - First Bad Version.py
+++ 0278 - First Bad Version.py
@@ -2,11 +2,12 @@ def isBadVersion(version):
     return version > 0
 
 
-class Solution(object):
+# Binary search. Time: O(lg n). Space: O(1)
+class Solution:
     def firstBadVersion(self, n):
         start, end = 1, n
         while True:
-            middle = (start + end) / 2
+            middle = (start + end) // 2
             if isBadVersion(middle):
                 if middle == 1 or not isBadVersion(middle - 1):
                     return middle
@@ -17,9 +18,22 @@ class Solution(object):
                 start = middle
 
 
+# Binary search. Time: O(lg n). Space: O(1)
+class Solution2:
+    def firstBadVersion(self, n):
+        low, high = 0, n
+        while low < high:
+            middle = low + (high - low) // 2
+            if isBadVersion(middle):
+                high = middle
+            else:
+                low = middle + 1
+        return low
+
+
 if __name__ == '__main__':
     import Test
 
-    Test.test(Solution().firstBadVersion, [
+    Test.test([Solution().firstBadVersion, Solution2().firstBadVersion], [
         (2, 1),
     ])
