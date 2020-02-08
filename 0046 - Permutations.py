9713dfef0f4643e1b7daced7fb7bc4c6f3ae1108
Jubing Chen <papalagi.chen@gmail.com>
2020-02-07 18:43:49 -0800

    Update "0046 - Permutations.py"

--- 0046 - Permutations.py
+++ 0046 - Permutations.py
@@ -1,26 +1,39 @@
 from typing import List
 
 
+# Recursive. Time: O(n!). Space: O(1)
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         results = []
-        self.p(nums, 0, results)
+        if len(nums) == 1:
+            results.append(nums)
+        for i in range(len(nums)):
+            for r in self.permute(nums[:i] + nums[i + 1:]):
+                results.append([nums[i]] + r)
         return results
 
-    def p(self, nums: List[int], left: int, results: List[List[int]]):
+
+# Backtracking. Time: O(n!). Space: O(1)
+class Solution2:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        return self.helper(nums, 0)
+
+    def helper(self, nums: List[int], left: int) -> List[List[int]]:
+        results = []
         if left == len(nums) - 1:
             results.append(nums.copy())
         else:
             for i in range(left, len(nums)):
                 nums[left], nums[i] = nums[i], nums[left]
-                self.p(nums, left + 1, results)
+                results.extend(self.helper(nums, left + 1))
                 nums[left], nums[i] = nums[i], nums[left]
+        return results
 
 
 if __name__ == '__main__':
     import Test
 
-    Test.test(Solution().permute, [
+    Test.test([Solution().permute, Solution2().permute], [
         ([1, 2, 3], [
             [1, 2, 3],
             [1, 3, 2],
@@ -29,4 +42,4 @@ if __name__ == '__main__':
             [3, 2, 1],
             [3, 1, 2],
         ]),
-    ])
+    ], sort_result=True)
