284ff68d10aa980772727fdd1d807d8e2fcf654e
Jubing Chen <papalagi.chen@gmail.com>
2020-02-16 11:14:47 -0800

    Update "0226 - Invert Binary Tree.py"

--- 0226 - Invert Binary Tree.py
+++ 0226 - Invert Binary Tree.py
@@ -1,10 +1,8 @@
 class Solution(object):
     def invertTree(self, root):
         if root:
-            if root.left:
-                self.invertTree(root.left)
-            if root.right:
-                self.invertTree(root.right)
+            self.invertTree(root.left)
+            self.invertTree(root.right)
             root.left, root.right = root.right, root.left
         return root
 
@@ -14,12 +12,5 @@ if __name__ == '__main__':
     import TreeBuilder
 
     Test.test(Solution().invertTree, [
-        (TreeBuilder.build({4: [{2: [{1: None},
-                                     {3: None}]},
-                                {7: [{6: None},
-                                     {9: None}]}]}),
-         TreeBuilder.build({4: [{7: [{9: None},
-                                     {6: None}]},
-                                {2: [{3: None},
-                                     {1: None}]}]})),
+        (TreeBuilder.deserialize('[4,2,7,1,3,6,9]'), TreeBuilder.deserialize('[4,7,2,9,6,3,1]')),
     ])
