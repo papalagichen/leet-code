4298cc8357d00757af37f208f27376038702f686
Jubing Chen <papalagi.chen@gmail.com>
2020-02-18 02:28:55 -0800

    Update "0198 - House Robber.py"

--- 0198 - House Robber.py
+++ 0198 - House Robber.py
@@ -1,14 +1,13 @@
+# Dynamic programming. Time: O(n). Space: O(1)
 class Solution:
     def rob(self, nums):
-        if len(nums) == 0:
-            return 0
-        elif len(nums) < 3:
-            return max(nums[:len(nums)])
-        else:
-            c1, c2, c3 = nums[0], nums[1], nums[0] + nums[2]
-            for n in range(3, len(nums)):
-                c1, c2, c3 = c2, c3, nums[n] + max(c1, c2)
-            return max(c1, c2, c3)
+        if len(nums) < 3:
+            return max(nums + [0])
+        # v[n] = max(v[n-2] + v[n], v[n-1])
+        c1, c2 = nums[0], max(nums[:2])
+        for n in nums[2:]:
+            c1, c2 = c2, max(c1 + n, c2)
+        return c2
 
 
 if __name__ == '__main__':
@@ -22,4 +21,5 @@ if __name__ == '__main__':
         ([3, 3, 2, 1], 5),
         ([1, 20, 3, 4, 15, 6], 35),
         ([2, 1, 1, 2], 4),
+        ([0, 1, 0], 1),
     ])
