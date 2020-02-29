0b1f16d41eca2d0f4aeea3e3005e3fdf43830ee3
Jubing Chen <papalagi.chen@gmail.com>
2020-02-28 21:38:37 -0800

    Update "0343 - Integer Break.py"

--- 0343 - Integer Break.py
+++ 0343 - Integer Break.py
@@ -1,3 +1,4 @@
+# Dynamic programming. Time: O(n). Space: O(n)
 class Solution:
     cache = {
         1: 1,
@@ -20,10 +21,28 @@ class Solution:
         return max_product
 
 
+# Calculate only 2 and 3. Time: O(1). Space: O(1)
+class Solution2:
+    def integerBreak(self, n: int) -> int:
+        a = n % 3
+        if n > 3:
+            result = 3 ** ((n // 3) - (1 if a == 1 else 0))
+        elif n < 3:
+            return 1
+        else:
+            return 2
+        if a == 1:
+            return result * 4
+        elif a == 2:
+            return result * 2
+        else:
+            return result
+
+
 if __name__ == '__main__':
     import Test
 
-    Test.test(Solution().integerBreak, [
+    Test.test([Solution().integerBreak, Solution2().integerBreak], [
         (1, 1),
         (2, 1),
         (3, 2),
