f0b83102ad7e239acebd54798344702b6c582b7e
Jubing Chen <papalagi.chen@gmail.com>
2021-02-16 09:18:11 -0800

    Update "0404 - Sum of Left Leaves.py"

--- 0404 - Sum of Left Leaves.py
+++ 0404 - Sum of Left Leaves.py
@@ -13,3 +13,24 @@ class Solution:
         if node.right:
             s += self.sumOfLeftLeaves(node.right)
         return s
+
+
+class Solution2:
+    def sumOfLeftLeaves(self, node: TreeNode) -> int:
+        if node is None:
+            return 0
+        if node.left is not None and node.left.left is None and node.left.right is None:
+            left = node.left.val
+        else:
+            left = self.sumOfLeftLeaves(node.left)
+        return left + self.sumOfLeftLeaves(node.right)
+
+
+if __name__ == '__main__':
+    import Test
+    from TreeBuilder import deserialize
+    from TreeBuilder import TreeNode
+
+    Test.test([Solution().sumOfLeftLeaves, Solution2().sumOfLeftLeaves], [
+        ((deserialize('[3,9,20,null,null,15,7]')), 24),
+    ])
