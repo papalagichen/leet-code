5089fbba6b4086513ae21a0c421d980560c11a0b
Jubing Chen <papalagi.chen@gmail.com>
2021-07-01 20:47:30 -0700

    Add "0022 - Generate Parentheses.py"

--- 0022 - Generate Parentheses.py
+++ 0022 - Generate Parentheses.py
@@ -1,19 +1,57 @@
+from typing import List
+
+"""
+# 1
+      1, 0
+(     0, 1
+()    0, 0
+
+# 2
+      2, 0
+(     1, 1
+((    0, 2
+(()   0, 1
+(())  0, 0
+()    1, 0
+()(   0, 1
+()()  0, 0
+
+"""
+
+
 class Solution(object):
-    def generateParenthesis(self, n):
-        """
-        :type n: int
-        :rtype: List[str]
-        """
-        
+    def generateParenthesis(self, n) -> List[str]:
+        return self.helper(n, 0)
+
+    def helper(self, n, m) -> List[str]:
+        res = []
+        if n > 0:
+            for x in self.helper(n - 1, m + 1):
+                res.append('(' + x)
+        if m > 0:
+            for x in self.helper(n, m - 1):
+                res.append(')' + x)
+        if n == m == 0:
+            res.append('')
+        return res
 
 
 if __name__ == '__main__':
     import Test
 
     Test.test(Solution().generateParenthesis, [
-        (3, ["((()))",
-             "(()())",
-             "(())()",
-             "()(())",
-             "()()()"]),
-    ])
+        (1, [
+            "()",
+        ]),
+        (2, [
+            "(())",
+            "()()",
+        ]),
+        (3, [
+            "((()))",
+            "(()())",
+            "(())()",
+            "()(())",
+            "()()()",
+        ]),
+    ], sort_result=True)
