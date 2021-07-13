16ee4c583d220d62f3209bae1dca286837e13180
Jubing Chen <papalagi.chen@gmail.com>
2021-07-12 20:22:37 -0700

    Add two more solutions to "0509 - Fibonacci Number.py"

--- 0509 - Fibonacci Number.py
+++ 0509 - Fibonacci Number.py
@@ -1,5 +1,20 @@
-# Dynamic programming. Time: O(n). Space: O(1)
+from typing import List
+
+"""
+0 1 1 2 3 5 8
+"""
+
+
+# Brute force. Time: O(2^n). Space: O(n)
 class Solution:
+    def fib(self, N: int) -> int:
+        if N < 2:
+            return N
+        return self.fib(N - 1) + self.fib(N - 2)
+
+
+# Dynamic programming iterative version. Time: O(n). Space: O(1)
+class Solution2:
     def fib(self, N: int) -> int:
         a, b = 0, 1
         for i in range(N):
@@ -7,8 +22,22 @@ class Solution:
         return a
 
 
+# Dynamic programming recursive version. Time: O(n). Space: O(1)
+class Solution3:
+    def fib(self, N: int) -> int:
+        mem = [None] * (N + 1)
+        return self.helper(N, mem)
+
+    def helper(self, N: int, mem: List[int]) -> int:
+        if N < 2:
+            return N
+        if mem[N] is None:
+            mem[N] = self.fib(N - 1) + self.fib(N - 2)
+        return mem[N]
+
+
 # Table lookup. Time: O(1). Space: O(n)
-class Solution2:
+class Solution4:
     def fib(self, N: int) -> int:
         return [
             0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711,
@@ -19,7 +48,7 @@ class Solution2:
 if __name__ == '__main__':
     import Test
 
-    Test.test([Solution().fib, Solution2().fib], [
+    Test.test([Solution().fib, Solution2().fib, Solution3().fib, Solution4().fib], [
         (0, 0),
         (1, 1),
         (2, 1),
