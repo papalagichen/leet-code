09a3ea612bc089151677d7d6ad42989cbfb146fb
Jubing Chen <papalagi.chen@gmail.com>
2021-07-12 20:36:16 -0700

    Add Solution2 to "0120 - Triangle.py"

--- 0120 - Triangle.py
+++ 0120 - Triangle.py
@@ -7,7 +7,7 @@ from typing import List
 """
 
 
-# Dynamic Programming. Time: O(n). Space: O(n)
+# Dynamic Programming top down. Time: O(n). Space: O(n)
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         min_totals = triangle[0]
@@ -24,10 +24,20 @@ class Solution:
         return min(min_totals)
 
 
+# Dynamic Programming bottom up. Time: O(n). Space: O(1)
+class Solution2:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        min_totals = triangle[-1]
+        for i in reversed(range(len(triangle) - 1)):
+            for j in range(len(triangle[i])):
+                min_totals[j] = triangle[i][j] + min(min_totals[j], min_totals[j + 1])
+        return min_totals[0]
+
+
 if __name__ == '__main__':
     import Test
 
-    Test.test([Solution().minimumTotal], [
+    Test.test([Solution().minimumTotal, Solution2().minimumTotal], [
         ([[2], [3, 4], [6, 5, 7], [4, 1, 8, 3]], 11),
         ([[-10]], -10),
     ])
