cb616748ba285538531140552eea0f5b1d65f776
Jubing Chen <papalagi.chen@gmail.com>
2021-07-27 13:13:23 -0700

    Update comment to "0300 - Longest Increasing Subsequence.py"

--- 0300 - Longest Increasing Subsequence.py
+++ 0300 - Longest Increasing Subsequence.py
@@ -1,45 +1,50 @@
 import bisect
 from typing import List
 
+"""
+10 9 2 5 3 7 101 18
+-------------------
+1
+   1
+     1
+       2
+         2
+           3
+             4
+                 4
+"""
 
-# Dynamic programming with time O(n^2) and space O(n)
+
+# Dynamic Programming. Time O(n^2). Space O(n)
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         if nums is None or len(nums) == 0:
             return 0
-
         mem = [0] * len(nums)
-
         for i in range(len(nums)):
             mem[i] = self.look_back(nums[i], nums[:i + 1], mem) + 1
-
         return max(mem)
 
     def look_back(self, target: int, nums: List[int], mem: List[int]) -> int:
         max_length = 0
-
         for i in range(len(nums)):
             if nums[i] < target:
                 max_length = max(max_length, mem[i])
-
         return max_length
 
 
-# Dynamic programming and binary search with time O(n lg n) and space O(n)
+# Dynamic Programming + Binary Search. Time O(n lg n). Space O(n)
 class Solution2:
     def lengthOfLIS(self, nums: List[int]) -> int:
         if nums is None or len(nums) == 0:
             return 0
-
         mem = []
-
         for n in nums:
             i = bisect.bisect_left(mem, n)
             if i >= len(mem):
                 mem.append(n)
             else:
                 mem[i] = n
-
         return len(mem)
 
 
